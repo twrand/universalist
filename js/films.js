1da2e12ac261692c508a2b257b7178fa40fa19ac
twrand <timothywrandolph@gmail.com>
2020-12-27 11:41:29 -0500

    Major booklist update with TGB to 200

---
+++ js/films.js
@@ -0,0 +1,5 @@
+// Source of the array of films used to create the film list
+var films = [
+	// HOLLYWOOD GOLDEN AGE
+	{ title:"Citizen Kane", director:"Welles", year:1941, done:true, genre:"golden", },
+];
---
+++ js/music.js
@@ -0,0 +1,5 @@
+// Source of the array of music used to create the music list
+var music = [
+	// ROCK AND ROLL
+	{ title:"Yellow Ledbetter", artist:"Pearl Jam", album:"Ten", year:1991, done:true, genre:"rock", },
+];
